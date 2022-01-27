50cd729732dfdc4894976a8b8d0002add1a2075a
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-01-27 21:44:48 +0000

    Added file for notes

---
+++ Fc_association_scripts/Thoughts.R
@@ -0,0 +1,16 @@
+#Thoughts ####
+
+#Route 1
+#Prune for LD
+#Take our most significant snps
+#Then look to see what they are in LD with in terms of SNPs with functional consequences
+#Include these in the analysis
+
+
+#Route2
+#Take sig SNPs and include functional snps that are in LD
+#Prune but prioritise, sig and functional snps
+
+#Route 3
+#Group SNPs that are in LD together and then do haplotype analysis 
+#I think do this anyways
