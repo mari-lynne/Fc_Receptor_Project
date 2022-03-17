326c3aee46827be0306724aef7795ce11898c79c
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-03-17 12:01:38 +0000

    Script for microarray Fc eqtl analysis

---
+++ Cis-EQTL/array_eqtl.R
@@ -0,0 +1,9 @@
+# eQTL analysis ###
+
+### Aims ###
+# Prepare data for eQTL analysis
+# Associate sig fc snps with fc gene expression and outcome
+# Visualise results
+# Do for microarray and rna-seq data sets separately (before working on data harmonisation)
+
+## Microarray data ###
