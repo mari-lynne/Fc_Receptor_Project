0ff5e539baf20e22031b80414213f5b7e96afc58
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-09-04 21:14:08 +0100

    #/Fixed ISSUE - labels not plotting for sig settings

--- DEG_analysis/volcano_function.R
+++ DEG_analysis/volcano_function.R
@@ -2,19 +2,18 @@
 
 plot_vol <- function(deg, p = 0.05, FC = 0,
                      lab_type= "top", genes, top = 20,
-                     gene_col = "gene_name", #colname with gene_IDs
                      title="", alpha = 0.98,
                      colours = c("#a50000","#800000","#ef5a3a","orange","yellow")) {
-
   
-#/ Checks
-if (missing(deg)){
-  stop("Error: deg arg is missing. Please provide a toptable data frame")} 
-#if (!missing(genes) & !is.character(genes)){
+  
+  #/ Checks
+  if (missing(deg)){
+    stop("Error: deg arg is missing. Please provide a toptable data frame")} 
+  #if (!missing(genes) & !is.character(genes)){
   #stop("Error: label is not a character vector")}
-if (!missing(lab_type) & lab_type != c('top') & missing(genes)) {
-  stop("Error: label requires character vector with selected genes")}
-
+  if (!missing(lab_type) & lab_type != c('top') & missing(genes)) {
+    stop("Error: label requires character vector with selected genes")}
+  
   #/ 1) Define sig values
   log_p <- -log10(p)
   #Adj p values #need to convert adjusted p-val to unadjusted for plot yscale
@@ -25,7 +24,7 @@ if (!missing(lab_type) & lab_type != c('top') & missing(genes)) {
   log_adj <- -log10(adj.p)
   
   ##/Set up reg table 2)
- 
+  
   deg <- deg %>%
     mutate(reg =
              case_when(
@@ -47,33 +46,39 @@ if (!missing(lab_type) & lab_type != c('top') & missing(genes)) {
     lab_data <- NULL #Empty dataframe, need to work out how not to error
     warning("No genes highlighted")
   } else if(lab_type == "sig"){
-      gene_label <- genes
-      lab_data <- deg[(deg$reg == "Sig P <0.05" |deg$reg == "Sig Adj. P <0.05" )
-                    & (deg$gene_col %in% genes),]
+    gene_label <- genes
+    lab_data <- deg[(deg$reg == "Sig P <0.05" |deg$reg == "Sig Adj. P <0.05" )
+                    & (deg$gene_name %in% genes),]
   } else if (lab_type == "ns"){
-        gene_label <- genes
-        lab_data <- deg[(deg$gene_col %in% genes),]}
-    else if (lab_type == "top"){
-          lab_data <- slice_min(deg,adj.P.Val,n=top)
-          gene_label <- lab_data$gene_col
+    gene_label <- genes
+    lab_data <- deg[(deg$gene_name %in% genes),]}
+  else if (lab_type == "top"){
+    lab_data <- slice_min(deg,adj.P.Val,n=top)
+    gene_label <- lab_data$gene_name
   }
 
-
-#Plot Volcano 4)
-vol <-
-  deg %>% ggplot(aes(x=logFC,y=-log10(P.Value),label=gene_name))+
-  geom_point(aes(color = P.Value, alpha=alpha))+
-  labs(title = title) +
-  theme_minimal() +theme(legend.position = "none") +
-  geom_hline(yintercept = log_p, linetype = 2.5, alpha =0.7) +
-  geom_hline(yintercept =log_adj, linetype = 2.5, alpha =0.7)+
-  geom_label_repel(data=lab_data,
-                   size=3.5,direction="both",nudge_y =1.6,nudge_x =0.1,
-                   angle= 70,vjust= 0,segment.size= 0.5,
-                   segment.color="#331002",fill="#f7f7f5")+
-  scale_color_gradientn(colours = colours,
-                        values=c(0,adj.p,p,1))
-return(vol)
+  #Plot Volcano 4)
+  vol <-
+    deg %>% ggplot(aes(x=logFC,y=-log10(P.Value),label=gene_name))+
+    geom_point(aes(color = P.Value, alpha=alpha))+
+    labs(title = title) +
+    theme_minimal() +theme(legend.position = "none") +
+    geom_hline(yintercept = log_p, linetype = 2.5, alpha =0.7) +
+    geom_hline(yintercept =log_adj, linetype = 2.5, alpha =0.7)+
+    geom_label_repel(data=lab_data,
+                     size=3.5,direction="both",nudge_y =1.6,nudge_x =0.1,
+                     angle= 70,vjust= 0,segment.size= 0.5,
+                     segment.color="#331002",fill="#f7f7f5")+
+    scale_color_gradientn(colours = colours,
+                          values=c(0,adj.p,p,1))
+  return(vol)
 } 
+  
+  
+  
+  
+#/ Error checking 5/9
+#/ ISSUE - labels not plotting for sig settings
 
-plot_vol(V1, lab = 'top', title = "24h Postvac")
+#replace all gene_col with gene_name
+#Updates works, fix gene col setting later
