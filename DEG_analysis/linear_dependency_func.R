cc34c65b0a251fd344ff70f668b9843d9d49b2a2
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-09-12 02:52:37 +0100

    added extra options

--- DEG_analysis/linear_dependency_func.R
+++ DEG_analysis/linear_dependency_func.R
@@ -20,7 +20,7 @@ a = c(1, 0, 0, 1)
 b = c(0, 1, 1, 0)
 c = c(1, 1, 0, 1)
 d = c(0, 0, 1, 1)
-dmat <- matrix(c(a, b, c, d),
+mat <- matrix(c(a, b, c, d),
               nrow = 4,
               ncol = 4,
               dimnames = list(c("a", "b", "c", "d")))
@@ -34,14 +34,24 @@ mat <-
     (x + 2)) #adding 2 to matrix to change example
 print(mat[, 1] + mat[, 2]) # Only one observation
 
-# Function 4
+# LD check Function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#filter - poss Option to filter ld columns from matrix
-# recode non-numeric vars to numeric so we can add :)
-
-ld_check <- function(mat) {
+ld_check <- function(mat,
+                     filter=c("1")) { #Default filter first col out of LDs
+                                      #To switch to remove 2nd col type 2
+  message("Default:  Filter = 1
+          If LDs are present, remove the first column
+          Filter = 2
+          If LDs are present, remove the second column
+          Filter = 3
+          No removal of LD or null columns")
+  
   #/ 1) Checks:
   # If mat is matrix; else convert
+  
+  #/ 1) Rank check
+  print(paste0("ncols = ",ncol(mat)))
+  print(paste0("rank = ",qr(mat)$rank))
   
   #/ 2) Matrix Addition
   ind <-
@@ -54,15 +64,86 @@ ld_check <- function(mat) {
   colnames(out) <- c_names # Update sum table colnames
   
   #/ 3) Validate unique observations 
-  for (i in seq_along(c_names)) {
-    if (length(unique(out[, i])) == 1) {
-      print(paste0("Linear dependent matrix columns =", c_names[i]))
+  for (i in 1:ncol(out)) {
+    if (filter == 1 && length(unique(out[, i])) == 1) {
+      print(paste0("Linear dependent cols =", c_names[i]))
+      x <- str_extract(c_names[1],"\\d")
+    } else if ((filter == 2||3) && length(unique(out[, i])) == 1) {
+      print(paste0("Linear dependent cols =", c_names[i]))
+      x <- str_extract(c_names[1],"\\d$")
     }
   }
+  
+  #/ 4) Highlight zero vars
+  for (i in 1:ncol(mat)){
+    if(sum(mat[,i]) == 0){
+      print(paste0("Null vectors present - col ",i))
+      y <- i
+    }
+  }
+  #/ 5) Highlight vars with only one observations
+  for (i in 1:ncol(mat)){
+    if(sum(mat[,i]) == 1){
+      print(paste0("Single observation - col ",i))
+       z <- i
+    }
+  }
+  
+  #/ 6) Column removal check
+  t1 <- c(x,y,z)
+  print(t1)
+  message(paste("removing problem columns: ", t1," from matrix"))
+  ind_mat <- mat[,t1]
+  print(paste0("Updated ncols = ",ncol(mat)))
+  print(paste0("Updated rank = ",qr(mat)$rank))
+  return(ind_mat)
 }
 
-dmat = mat
+
 ld_check(mat=dmat)
-debug(ld_check)
-  
- 
+
+#debug(ld_check)
+
+mat[,c(x,y,z)]
+
+t <- c(x,y,z)
+
+mat[,t]
+
+paste0(qt, t, c(qt,//,))
+rm(z)
+
+
+addQuotes_<-function(str,qt="'"){
+  return(strp<-paste(qt,str,qt,sep=''));
+}
+
+
+
+#Updates 
+
+#filter - poss Option to filter ld columns from matrix
+# recode non-numeric vars to numeric so we can add :)
+# If there are lots of LD columns - just show the most common denominator
+# If col x or + by other col (pairwise) = value of column in matrix remove
+# Null check
+# Rank check
+colSums(mat) == 0
+
+#Testing ####
+a = c(1, 0, 0, 1)
+b = c(0, 1, 1, 0)
+c = c(1, 1, 0, 1)
+d = c(0, 0, 1, 1)
+mat <- matrix(c(a, b, c, d),
+               nrow = 4,
+               ncol = 4,
+               dimnames = list(c("a", "b", "c", "d")))
+mat <- cbind(mat,(c(0,0,0,0)))
+mat <- cbind(mat,(c(0,0,0,1)))
+
+str_extract(c_names[1],"\\d") #just take first mismatch by default
+#will therefore remove first col
+#change to second d as option
+str_extract(c_names[1],"\\d$") #works :)
+
